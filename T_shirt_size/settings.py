46b85537000caa8009f560aed1c2e6728e95fc56
vijaykumar <vijay10022@gmail.com>
2020-05-11 18:39:37 +0530

    Done

--- T_shirt_size/settings.py
+++ T_shirt_size/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'T_shirt',
 ]
 
 MIDDLEWARE = [
--- T_shirt_size/urls.py
+++ T_shirt_size/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from T_shirt import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.predict,name="predict"),
 ]
